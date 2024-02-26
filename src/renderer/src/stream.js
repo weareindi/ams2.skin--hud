4a7fc6fbf727f7109580946e18d893b18b7a3ea3
Laurence Archer <laurence@weareindi.co.uk>
2024-02-26 17:22:28 +0000

    Add missing stream.js file to repo

--- src/renderer/src/stream.js
+++ src/renderer/src/stream.js
@@ -11,8 +11,12 @@ import SvgComponent from './views/components/SvgComponent.vue';
 // Main CSS
 import './assets/scss/_main.scss';
 
+// Workers
+import StreamWorkerMainThread from './workers/StreamWorker/StreamWorkerMainThread';
+
 // app
 const app = createApp(stream);
 app.use(router);
+app.use(new StreamWorkerMainThread());
 app.component('SvgComponent', SvgComponent);
 app.mount('#stream');
